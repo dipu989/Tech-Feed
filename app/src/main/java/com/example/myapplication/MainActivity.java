e32cac141650f5e9cbd30a9ebeee86ef1af5e9fc
Shantnu Kumar <me.shantnu@gmail.com>
2019-02-07 08:06:37 +0530

    Add main screen with download tasks code.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,13 +1,88 @@
 package com.example.myapplication;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> titles = new ArrayList<>();
+
+    ArrayAdapter arrayAdapter;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DownloadTask task = new DownloadTask();
+        try{
+            task.execute("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty");
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        ListView listView = findViewById(R.id.listView);
+
+        arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,titles);
+
+        listView.setAdapter(arrayAdapter);
+
+
+
+    }
+
+    public class DownloadTask extends AsyncTask<String, Void, String>{
+
+        @Override
+        protected String doInBackground(String... urls) {
+
+            URL url;
+            String result = "";
+            HttpURLConnection urlConnection = null;
+
+            try{
+                url = new URL(urls[0]);
+
+                urlConnection = (HttpURLConnection)url.openConnection();
+
+                InputStream inputStream = urlConnection.getInputStream();
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+
+                int data = inputStreamReader.read();
+
+                while(data!=-1)
+                {
+                    char current = (char)data;
+
+                    result += current;
+
+                    data = inputStreamReader.read();
+                }
+
+                Log.i("URL info ",result);
+
+                return  result;
+
+            }catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
     }
 }
